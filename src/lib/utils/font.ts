2a524d0d83856629d46f19033d0a6d2cf0fe3ae2
pphatdev <leatsophat02@gmail.com>
2025-06-18 04:16:10 +0700

    feat: update next.config.ts for image remote patterns and add schema dependencies

--- src/lib/utils/font.ts
+++ src/lib/utils/font.ts
@@ -1,20 +1,5 @@
-import { Aladin, Kantumruy_Pro, Noto_Color_Emoji, Open_Sans, Poppins, Srisakdi } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-export const aladin = Aladin({
-    variable: "--font-aladin",
-    weight: ["400"],
-    subsets: ["latin"],
-    display: "swap",
-    preload: true,
-});
-
-export const srisakdi = Srisakdi({
-    variable: "--font-srisakdi",
-    weight: ["400"],
-    subsets: ["latin"],
-    display: "swap",
-    preload: true,
-});
 
 export const poppins = Poppins({
     variable: "--font-poppins",
@@ -24,26 +9,18 @@ export const poppins = Poppins({
     preload: true,
 });
 
-export const kantumruyPro = Kantumruy_Pro({
-    variable: "--font-kantumruy",
-    weight: ["400", "500", "600", "700"],
-    subsets: ["latin"],
-    display: "swap",
-    preload: true,
-});
+// export const kantumruyPro = Kantumruy_Pro({
+//     variable: "--font-kantumruy",
+//     weight: ["400", "500", "600", "700"],
+//     subsets: ["latin"],
+//     display: "swap",
+//     preload: true,
+// });
 
-export const openSans = Open_Sans({
-    variable: "--font-open-sans",
-    weight: ["400", "500", "600", "700", "800"],
-    subsets: ["latin"],
-    display: "swap",
-    preload: true,
-});
-
-export const notoColorEmoji = Noto_Color_Emoji({
-    variable: "--font-noto-emoji",
-    weight: ["400"],
-    subsets: ["emoji"],
-    display: "swap",
-    preload: true,
-});
+// export const openSans = Open_Sans({
+//     variable: "--font-open-sans",
+//     weight: ["400", "500", "600", "700", "800"],
+//     subsets: ["latin"],
+//     display: "swap",
+//     preload: true,
+// });
