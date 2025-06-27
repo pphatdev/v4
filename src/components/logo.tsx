7caa3cbaaf8649082b9ba0e9bc2b511ad53cc4f7
pphatdev <pphat.kits@gmail.com>
2025-06-27 10:14:46 +0700

    ✨ feat: restructure Logo component layout for improved styling and organization

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 
 export const Logo = () => {
     return (
-        <Link href="/" className="shrink-0 items-center flex flex-col z-50" aria-label="Home">
-            <Image width={32} height={32} src={'/assets/logo/logo-transparent-dark-mode.png'} alt={"Logo"} className="hidden dark:block" />
-            <Image width={32} height={32} src={'/assets/logo/logo-transparent-light-mode.png'} alt={"Logo"} className="dark:hidden" />
-            <Badge className="py-0.5 pt-1 h-fit -translate-y-3 bg-background text-[8px] uppercase" variant={"outline"}>PPhat</Badge>
-        </Link>
+        <div className="w-12">
+            <Link href="/" className="items-center flex flex-col z-50 justify-start border rounded-xl py-2 border-primary/50" aria-label="Home">
+                <Image width={32} height={32} src={'/assets/logo/logo-transparent-dark-mode.png'} alt={"Logo"} className="hidden dark:block" />
+                <Image width={32} height={32} src={'/assets/logo/logo-transparent-light-mode.png'} alt={"Logo"} className="dark:hidden" />
+                {/* <Badge className="py-0.5 pt-1 h-fit -translate-y-3 bg-background text-[8px] uppercase" variant={"outline"}>PPhat</Badge> */}
+            </Link>
+        </div>
     );
 };
